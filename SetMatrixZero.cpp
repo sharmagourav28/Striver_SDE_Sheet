3bce14849d80de2d5cc07a4741aa6cfe43f7184a
Gourav sharma <gouravsharma2806@gmail.com>
2023-09-10 14:25:19 +0530

    new file:   SetMatrixZero.cpp

---
+++ SetMatrixZero.cpp
@@ -0,0 +1,25 @@
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int col[m] = {0};
+    int row[n] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            row[i] = 1;
+            col[j] = 1;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (row[i] || col[j])
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
